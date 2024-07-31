2ce3b777db8d05fd1be35caa3a6bb5c348545086
POPLE-oO <poplepop111@gmail.com>
2024-07-31 16:30:10 +0900

    update neovim -> <C-w><C-w>でターミナルを起動できるようにした

--- settings/.config/nvim/lua/keymaps.lua
+++ settings/.config/nvim/lua/keymaps.lua
@@ -1,5 +1,23 @@
--- Esc to jj
-vim.keymap.set("i", "jj", "<Esc>", { silent = true, noremap = true})
-
 -- delete char of right side from cursor with <C-l>
-vim.keymap.set("i", "<C-l>", "<Del>", {silent = true, noremap = true})
+vim.keymap.set("i", "<C-l>", "<Del>", { silent = true, noremap = true })
+
+-- <C-k> to backword enter
+vim.keymap.set("i", "<C-k>", "<Esc>O", { silent = true, noremap = true })
+
+-- <C-w>t enter terminal
+vim.keymap.set("n", "<C-w><C-t>", ":Openterm<CR>", { silent = true, noremap = true })
+-- <C-w><C-w> enter terminal
+vim.keymap.set("n", "<C-w><C-w>", ":Openterm<CR>", { silent = true, noremap = true })
+
+-- <C-[> to escape from terminal insert mode
+vim.keymap.set("t", "<C-[>", "<C-\\><C-n>", { silent = true, noremap = true })
+
+-- <C-w><C-t> to close terminal
+vim.keymap.set("t", "<C-w><C-t>", "<C-\\><C-n>:q<CR>", { silent = true, noremap = true })
+-- <C-w><C-w> to close terminal
+vim.keymap.set("t", "<C-w><C-w>", "<C-\\><C-n>:q<CR>", { silent = true, noremap = true })
+
+-- <C-w><C-k> move up window
+vim.keymap.set("t", "<C-w><C-k>", "<C-\\><C-n><C-w>k", { silent = true, noremap = true })
+-- <C-w>k move up window
+vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { silent = true, noremap = true })
--- settings/.config/nvim/lua/options.lua
+++ settings/.config/nvim/lua/options.lua
@@ -1,18 +1,48 @@
 --------------------
 --	basic settings
 --------------------
-vim.opt.tabstop = 4 		-- set tab width 4chars
+vim.opt.tabstop = 4 -- set tab width 4chars
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 
-vim.opt.number = true 		-- display line number
+vim.opt.number = true -- display line number
 
 -- display hidden chars
-vim.opt.listchars = 'tab:|> ,trail:･'
+vim.opt.listchars = "tab:|> ,trail:･"
 vim.opt.list = true
 
 -- enable mouse
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- enable clipboard
-vim.opt.clipboard:append({unnamedeplus = true})
+vim.opt.clipboard:append({ unnamedeplus = true })
+
+--------------------
+--	auto commands
+--------------------
+-- run at startup
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		-- write your own startup commands
+	end,
+})
+
+--------------------
+--	original commands
+--------------------
+-- open terminal
+-- -> <C-w>t <C-w><C-w>に設定しておく
+vim.api.nvim_create_user_command("Openterm", function()
+	-- ターミナルを画面の下側に起動
+	vim.cmd("below split")
+	vim.cmd("term")
+
+	-- ターミナルの大きさを高さ10に
+	vim.api.nvim_win_set_height(0, 10)
+
+	-- 起動後ターミナル内でinsertモードに
+	vim.cmd("startinsert")
+
+	-- ターミナル起動後元のファイルに戻る
+	-- vim.cmd("winc k")
+end, {})
